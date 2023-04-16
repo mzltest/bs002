06517e6855c20ebde8d842e29e00f1e8963e9015
mzltest <52124882+mzltest@users.noreply.github.com>
2023-04-16 06:46:15 +0000

    qqqq

--- todos/create/index.js
+++ todos/create/index.js
@@ -3,16 +3,14 @@ let data = require('@begin/data')
 
 exports.handler = async function create(req) {
   let todo = arc.http.helpers.bodyParser(req)
-  todo.created = Date.now()
-  await data.set({
-    table: 'todos',
-    ...todo
-  })
-  return {
-    statusCode: 302,
-    headers: {
-      location: '/',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    }
+  tdata=await data.get({table:'events','key':req.pathParameters.echannel})
+  if(!data){
+    t=await data.set({table:'events','key':req.pathParameters.echannel,events:[{t:Date.now(),m:todo.out}],finish:todo.finish})
   }
+  else{
+    tdata.finish=todo.finish
+    tdata.events=tdata.events.push({t:Date.now(),m:todo.out})
+    s=await data.set(tdata)
+  }
+  return {'ok':'true'}
 }
